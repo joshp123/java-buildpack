99b9168989e40d77b976fde72b11c31d10779099
Josh Palmer <Josh.Palmer@mendix.com>
2018-05-24 13:09:42 +0200

    temporarily disable adding agent so i can get ssh to container

--- lib/java_buildpack/framework/sentry_agent.rb
+++ lib/java_buildpack/framework/sentry_agent.rb
@@ -49,8 +49,8 @@ module JavaBuildpack
 
       # (see JavaBuildpack::Component::BaseComponent#release)
       def release
-        @droplet.java_opts
-            .add_javaagent(@droplet.sandbox + 'sentry.so')
+          return true
+        # @droplet.java_opts.add_javaagent(@droplet.sandbox + 'sentry.so')
       end
 
       protected
